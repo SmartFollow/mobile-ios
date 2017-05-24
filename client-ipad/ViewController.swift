af773fcb0d533dc7a138b66dd211608b99cdb8f4
Alexandre Page <alexandre.page@epitech.eu>
2017-05-24 15:32:59 +0100

    Initial Commit

---
+++ client-ipad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  client-ipad
+//
+//  Created by Alexandre Page on 24/05/2017.
+//  Copyright © 2017 Alexandre Page. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
