c2a51569c0cfe01cb9cd4816d3dbf51731f97fc7
Alexandre Page <alexandre.page@epitech.eu>
2017-06-10 19:03:47 +0100

    podfile

---
+++ client-ipad/LeftViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LeftViewController.swift
+//  client-ipad
+//
+//  Created by Alexandre Page on 10/06/2017.
+//  Copyright © 2017 Alexandre Page. All rights reserved.
+//
+
+import Foundation
